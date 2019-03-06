1c9b4d33be03f5c532f68fcda12d634e4571f5af
ldd <leonardoldd@gmail.com>
2019-03-06 12:37:34 -0500

    perf(index,charts): improve page performance
    - move scripts to body in html files
    - do not use `window.onload` event

--- src/index.mjs
+++ src/index.mjs
@@ -2,9 +2,9 @@ import { createFileSelectors } from "./modules/file.mjs";
 import { addNavbar, addFooter } from "./templates/helper.mjs";
 import { addUserListSelection } from "./templates/userListSelection.mjs";
 
-window.onload = () => {
+document.addEventListener("DOMContentLoaded", () => {
   addNavbar();
   addFooter();
   createFileSelectors();
   addUserListSelection();
-};
+});
