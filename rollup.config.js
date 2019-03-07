39343b32210cda5fac043a90392366d50646d437
ldd <leonardoldd@gmail.com>
2019-03-06 22:25:15 -0500

    build(rollup): copy files needed for distribution

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,36 @@
 // rollup.config.js
 import { terser } from "rollup-plugin-terser";
+import copy from "rollup-plugin-copy";
 
-export default {
-  input: "src/charts.mjs",
-  output: {
-    dir: "dist",
-    format: "esm"
+export default [
+  {
+    input: "src/index.mjs",
+    output: {
+      file: "dist/index.mjs",
+      format: "esm"
+    },
+    plugins: [
+      terser(),
+      copy({
+        // index files
+        "src/index.html": "dist/index.html",
+        "src/index.css": "dist/index.css",
+        "src/style.css": "dist/style.css",
+        "src/favicon.ico": "dist/favicon.ico",
+        // charts files
+        "src/charts.html": "dist/charts.html",
+        "src/charts.css": "dist/charts.css",
+        "src/data/": "dist/data/",
+        verbose: true
+      })
+    ]
   },
-  plugins: [terser()]
-};
+  {
+    input: "src/charts.mjs",
+    output: {
+      file: "dist/charts.mjs",
+      format: "esm"
+    },
+    plugins: [terser()]
+  }
+];
