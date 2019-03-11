2e8a3b1213a92fe3ec01f46ba8d0a5a8df8fce87
ldd <leonardoldd@gmail.com>
2019-03-11 18:13:13 -0400

    feat(templates, notification): add more notification configs and types
    - add danger notifications (and ability to use other notification types)
    - add custom text for when a notification is done

--- src/templates/common/notification.mjs
+++ src/templates/common/notification.mjs
@@ -1,37 +1,40 @@
 import { html } from "https://unpkg.com/lit-html@1.0.0/lit-html.js?module";
+import { ifDefined } from "https://unpkg.com/lit-html@1.0.0/directives/if-defined.js?module";
 import { getElement, addElement } from "../helper.mjs";
 
 let closeHandler = () => {};
 
-// TODO: lit-html needs to be updated to better handle conditional attributes
-const progressTemplate = value =>
-  value
-    ? html`
-        <progress class="progress is-success" value="${value}" max="100">
-          ${value}%
-        </progress>
-      `
-    : html`
-        <progress class="progress is-success" max="100"></progress>
-      `;
+const progressTemplate = ({ progressValue, type }) => {
+  const progressType = type === "" ? "is-success" : "is-danger";
+  return html`
+    <progress
+      class="progress ${progressType}"
+      value="${ifDefined(progressValue)}"
+      max="100"
+    >
+      ${progressValue}%
+    </progress>
+  `;
+};
 
-const notificationTemplate = (state = {}) => {
+const notificationTemplate = (state = {}, config = {}) => {
   const { isActive = true, length, expectedLength } = state;
-  let progress;
+  const { type = "", text: doneText = "Done!" } = config;
   let text = "Processing...";
+  let progressValue;
   if (length && expectedLength > 0) {
-    progress = Math.round((length / expectedLength) * 100);
+    progressValue = Math.round((length / expectedLength) * 100);
     text =
-      progress < 100
+      progressValue < 100
         ? `Fetching (${length}/${expectedLength}) entries`
-        : "Done!";
+        : doneText;
   }
   return isActive
     ? html`
-        <div id="notification-progress" class="notification is-small">
+        <div id="notification-progress" class="notification ${type} is-small">
           <button class="delete" @click=${closeHandler}></button>
           <p style="margin-bottom:0.5em">${text}</p>
-          ${progressTemplate(progress)}
+          ${progressTemplate({ progressValue, type })}
         </div>
       `
     : html``;
@@ -45,14 +48,35 @@ export function addNotification() {
 export function updateNotification({
   length,
   expectedLength,
-  isActive = true
+  isActive = true,
+  text,
+  type
 } = {}) {
   const parent = getElement("notification-container", "div", "");
-  addElement(notificationTemplate, parent, {
-    isActive,
-    length,
-    expectedLength
-  });
+  addElement(
+    notificationTemplate,
+    parent,
+    {
+      isActive,
+      length,
+      expectedLength
+    },
+    { type, text }
+  );
+}
+
+export function updateNotificationDanger({ text, isActive = true } = {}) {
+  const parent = getElement("notification-container", "div", "");
+  addElement(
+    notificationTemplate,
+    parent,
+    {
+      isActive,
+      length: 1,
+      expectedLength: 1
+    },
+    { type: "is-danger", text }
+  );
 }
 
 closeHandler = () => {
